ec723424665186b903dc57648d6d9615ee0b49dd
glock <glockman2309@gmail.com>
2025-09-12 23:18:22 +0530

    suffix

--- lala.cpp
+++ lala.cpp
@@ -3,35 +3,36 @@ using namespace std;
 #define long long long
 void help()
 {
-    long n,q;
-    cin >> n >> q;
-    long sum = 0;
+    long n,k;
+    cin >> n >> k;
     vector<long> vec;
-    vector<long> prefix(n+1, 0);
-    for(int i = 1; i <= n; i++)
+    vector<long> suffix(n+1);
+    for(int i = 0; i < n ; i++)
     {
         long num;
         cin >> num;
-        sum += num;
         vec.push_back(num);
-        prefix[i] = prefix[i-1] + vec[i-1];
     }
-    for(int i = 0; i < q; i++)
+    sort(vec.begin(),vec.end());
+    suffix[n] = 0;
+    suffix[n-1] = vec[n-1];
+    for(int i = n-2; i >= 0; i--)
     {
-        long l,r,k;
-        cin >> l >> r >> k;
-        long sum2 = prefix[r] - prefix[l-1];   
-        long n2 = (r-l+1)*k;
-        // cout << sum << " " << sum2 << " " << n2 << endl;
-        if((sum - sum2 + n2) % 2 == 1)
-        {
-            cout << "YES" << endl;
-        }
-        else
-        {
-            cout << "NO" << endl;
-        }
+        suffix[i] = suffix[i+1] + vec[i];
     }
+    long i = 0;
+    long j = n-k;
+    long max2 = 0;
+    while(k-- >= 0 )
+    {
+        long s = suffix[i] - suffix[j];
+        // cout<<suffix[i]<<" "<<suffix[j]<<endl;
+        // cout<<s<<endl;
+        max2 = max(s,max2);
+        i += 2;
+        j +=1;
+    }
+    cout << max2 << endl;
 }
 
 int main()
@@ -40,9 +41,34 @@ int main()
     cin.tie(nullptr);
     int t;
     cin >> t;
+    // t= 1;
     while (t--)
     {
         help();
     }
     return 0;
 }
+
+    // int j = n-1;
+    // // int summin = 0; 
+    // // int summax = 0;
+    // while((k--) && (i <= j))
+    // {
+    //     int num = vec[i] + vec[i+1];
+    //     if(num >= vec[j])
+    //     {
+    //         // summax += vec[j];
+    //         j = j-1;
+    //     }
+    //     else
+    //     {
+    //         // summin += num;
+    //         i = i + 2;
+    //     }
+    // }
+    // long sum = 0;
+    // for(i; i <= j; i++)
+    // {
+    //     sum += vec[i];
+    // }
+    // cout << sum << endl;
